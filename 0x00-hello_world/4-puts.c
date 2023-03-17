02d9d7e06b320a49fa80199e0de25b9026d71a0b
Th3rone <ayoub.azouzi@hotmail.com>
2023-03-16 19:51:18 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 
 
 /**
+ *
  * main - prints "Programming is like building a multilingual puzzle.
  * return Always 0.
  */
@@ -10,5 +11,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	
-	return(0);
+	return (0);
 }
